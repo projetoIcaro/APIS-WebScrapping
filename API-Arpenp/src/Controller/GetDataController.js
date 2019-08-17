cefef6118a04d7b4c5071d5d1c437e512fd979c3
Renan Ventura <renan.ventura.silva.st@everis.com>
2019-08-17 01:41:01 -0300

    criação da API Arpenp

---
+++ API-Arpenp/src/Controller/GetDataController.js
@@ -0,0 +1,33 @@
+const Page = require('../Components/homePage')
+
+module.exports = {
+    async GetData(req, res){
+        let page
+
+        try {
+            let v=1
+            if(v==1){
+                page = new Page()
+                let numeroProcesso = req.body.numeroProcesso
+
+                await page.visit('http://ec2-18-231-116-58.sa-east-1.compute.amazonaws.com/arpensp/login.html')
+                await page.findBotaoCrc()
+                await page.findBotaoMenu()
+                await page.findBotaoBusca()
+                await page.findBotaoRegistro()
+                await page.findBotaoVara()
+                await page.findBotaoVaraClick()
+                await page.findBotaoProcesso()
+                
+                let jsonResult = await page.ColetaDadosFinais()
+                return res.json(JSON.parse(jsonResult))
+            }
+    
+            return res.json(JSON.parse(`{"Status":"Falhou", "error":"Dados de busca invalidos"}`))
+        }catch(err){
+            console.log(err)
+            return res.json(JSON.parse(`{"Status":"Falhou", "error":${err}}`))
+        }
+
+    }
+}
